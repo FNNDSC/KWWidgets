148483f9c02c1564b06341c2ad58e1da21d28f6b
avila <avila>
2000-09-14 18:12:42 +0000

    tweaks in LOD rendering

--- vtkKWVolumeComposite.cxx
+++ vtkKWVolumeComposite.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkKWVolumeComposite.cxx,v $
   Language:  C++
-  Date:      $Date: 2000-09-13 15:45:16 $
-  Version:   $Revision: 1.18 $
+  Date:      $Date: 2000-09-14 18:12:42 $
+  Version:   $Revision: 1.19 $
 
 Copyright (c) 1998-1999 Kitware Inc. 469 Clifton Corporate Parkway,
 Clifton Park, NY, 12065, USA.
@@ -104,8 +104,7 @@ vtkKWVolumeComposite::vtkKWVolumeComposite()
   this->VolumeProperty->SetSpecularPower(1.0);
   this->VolumeProperty->ShadeOff();
   this->RayCastMapper->SetVolumeRayCastFunction(this->Composite);
-
-
+  
   this->HiResTextureID = 
     this->LODVolume->AddLOD( this->HiResTextureMapper,
 			     this->VolumeProperty, 0.0 );
@@ -179,13 +178,13 @@ vtkKWVolumeComposite::~vtkKWVolumeComposite()
   this->MedResTextureMapper->Delete();
   this->HiResTextureMapper->Delete();
   this->VolumeProperty->Delete();
+  this->LowResVolumeProMapper->Delete();
   
   this->LowResResampler->Delete();
   this->MedResResampler->Delete();
-  this->VProResampler->Delete();
-  
-  this->LowResVolumeProMapper->Delete();
+
   this->VolumeProMapper->Delete();
+  this->VProResampler->Delete();
 }
 
 void vtkKWVolumeComposite::SetInput(vtkImageData *input)
@@ -459,5 +458,5 @@ void vtkKWVolumeComposite::SerializeRevision(ostream& os, vtkIndent indent)
 {
   vtkKWComposite::SerializeRevision(os,indent);
   os << indent << "vtkKWVolumeComposite ";
-  this->ExtractRevision(os,"$Revision: 1.18 $");
+  this->ExtractRevision(os,"$Revision: 1.19 $");
 }
