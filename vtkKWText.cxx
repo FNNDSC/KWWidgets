8e6e98ec5a52c4af5f8930bd45e621a1d7770b1d
andy <andy>
2002-07-23 17:30:12 +0000

    Widget should be able to take 0 as the second argument

--- vtkKWText.cxx
+++ vtkKWText.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkKWText.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-01-11 18:35:24 $
-  Version:   $Revision: 1.7 $
+  Date:      $Date: 2002-07-23 17:30:12 $
+  Version:   $Revision: 1.8 $
 
 Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
 Clifton Park, NY, 12065, USA.
@@ -91,7 +91,7 @@ void vtkKWText::Create(vtkKWApplication *app, const char *args)
 
   // create the top level
   wname = this->GetWidgetName();
-  this->Script("text %s %s",wname,args);
+  this->Script("text %s %s",wname,(args?args:""));
 }
 
 
