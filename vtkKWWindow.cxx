194f82e4af3fda9692450f0893146a8f67776a3b
barre <barre>
2002-09-10 14:16:34 +0000

    ENH: the status image is a now a button that brings the "About..." dialog

--- vtkKWWindow.cxx
+++ vtkKWWindow.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkKWWindow.cxx,v $
   Language:  C++
-  Date:      $Date: 2002-09-09 21:30:41 $
-  Version:   $Revision: 1.115 $
+  Date:      $Date: 2002-09-10 14:16:34 $
+  Version:   $Revision: 1.116 $
 
 Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
 Clifton Park, NY, 12065, USA.
@@ -68,7 +68,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define VTK_KW_SHOW_PROPERTIES_LABEL "Show Left Panel"
 #define VTK_KW_EXIT_DIALOG_NAME "ExitApplication"
 
-vtkCxxRevisionMacro(vtkKWWindow, "$Revision: 1.115 $");
+vtkCxxRevisionMacro(vtkKWWindow, "$Revision: 1.116 $");
 vtkCxxSetObjectMacro(vtkKWWindow, PropertiesParent, vtkKWWidget);
 
 class vtkKWWindowMenuEntry
@@ -613,10 +613,12 @@ void vtkKWWindow::Create(vtkKWApplication *app, char *args)
   this->Script("image create photo");
   this->StatusImageName = vtkString::Duplicate(interp->result);
   this->CreateStatusImage();
-  this->StatusImage->Create(app,"label",
+  this->StatusImage->Create(app,"button",
                             "-relief sunken -bd 1 -fg #ffffff -bg #ffffff");
-  this->Script("%s configure -image %s", this->StatusImage->GetWidgetName(),
+  this->Script("%s configure -image %s", 
+               this->StatusImage->GetWidgetName(),
                this->StatusImageName);
+  this->StatusImage->SetCommand(this, "DisplayAbout");
   this->Script("pack %s -side left -ipadx 2 -ipady 2", 
                this->StatusImage->GetWidgetName());
 
@@ -1145,7 +1147,7 @@ void vtkKWWindow::SerializeRevision(ostream& os, vtkIndent indent)
 {
   vtkKWWidget::SerializeRevision(os,indent);
   os << indent << "vtkKWWindow ";
-  this->ExtractRevision(os,"$Revision: 1.115 $");
+  this->ExtractRevision(os,"$Revision: 1.116 $");
 }
 
 int vtkKWWindow::ExitDialog()
