816c84c4cf526acc58b862a0c5925c36f33212bc
andy <andy>
2003-04-22 18:23:11 +0000

    ERR: Revert back to the pragmas, so that it works with VTK 4.2.2

--- vtkKWEntry.cxx
+++ vtkKWEntry.cxx
@@ -3,8 +3,8 @@
   Program:   Visualization Toolkit
   Module:    $RCSfile: vtkKWEntry.cxx,v $
   Language:  C++
-  Date:      $Date: 2003-04-18 19:35:59 $
-  Version:   $Revision: 1.24 $
+  Date:      $Date: 2003-04-22 18:23:11 $
+  Version:   $Revision: 1.25 $
 
 Copyright (c) 2000-2001 Kitware Inc. 469 Clifton Corporate Parkway,
 Clifton Park, NY, 12065, USA.
@@ -47,8 +47,17 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "vtkKWIcon.h"
 #include "vtkKWListBox.h"
 
-#include <vtkstd/vector>
-#include <vtkstd/string>
+#ifdef _MSC_VER
+#pragma warning (push, 1)
+#pragma warning (disable: 4702)
+#endif
+
+#include <vector>
+#include <string>
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif
 
 //----------------------------------------------------------------------------
 //============================================================================
@@ -70,7 +79,7 @@ public:
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWEntry );
-vtkCxxRevisionMacro(vtkKWEntry, "$Revision: 1.24 $");
+vtkCxxRevisionMacro(vtkKWEntry, "$Revision: 1.25 $");
 
 //----------------------------------------------------------------------------
 vtkKWEntry::vtkKWEntry()
