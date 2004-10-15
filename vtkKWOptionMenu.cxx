904154c0fba303018395ae0e023804b1b961d063
barre <barre>
2004-10-15 14:21:54 +0000

    BUG: widget has to be created to call this method

--- vtkKWOptionMenu.cxx
+++ vtkKWOptionMenu.cxx
@@ -20,7 +20,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWOptionMenu );
-vtkCxxRevisionMacro(vtkKWOptionMenu, "$Revision: 1.31 $");
+vtkCxxRevisionMacro(vtkKWOptionMenu, "$Revision: 1.32 $");
 
 //----------------------------------------------------------------------------
 vtkKWOptionMenu::vtkKWOptionMenu()
@@ -75,8 +75,11 @@ void vtkKWOptionMenu::SetCurrentEntry(const char *name)
 //----------------------------------------------------------------------------
 void vtkKWOptionMenu::SetCurrentImageEntry(const char *image_name)
 { 
-  this->Script("%s configure -image %s", this->GetWidgetName(), image_name);
-  this->SetValue(image_name);
+  if (this->IsCreated())
+    {
+    this->Script("%s configure -image %s", this->GetWidgetName(), image_name);
+    this->SetValue(image_name);
+    }
 }
  
 //----------------------------------------------------------------------------
