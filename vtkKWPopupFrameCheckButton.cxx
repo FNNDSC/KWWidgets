0e49fb944a3cde901ae634940d218426218910c4
barre <barre>
2005-03-20 00:18:10 +0000

    ENH: clean-up and sync the API of all the seperate labeled core widgets and set of core widgets

--- vtkKWPopupFrameCheckButton.cxx
+++ vtkKWPopupFrameCheckButton.cxx
@@ -15,13 +15,13 @@
 
 #include "vtkKWCheckButton.h"
 #include "vtkKWFrame.h"
-#include "vtkKWLabeledFrame.h"
+#include "vtkKWFrameLabeled.h"
 #include "vtkKWPopupButton.h"
 #include "vtkObjectFactory.h"
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWPopupFrameCheckButton );
-vtkCxxRevisionMacro(vtkKWPopupFrameCheckButton, "$Revision: 1.3 $");
+vtkCxxRevisionMacro(vtkKWPopupFrameCheckButton, "$Revision: 1.4 $");
 
 int vtkKWPopupFrameCheckButtonCommand(ClientData cd, Tcl_Interp *interp,
                                       int argc, char *argv[]);
