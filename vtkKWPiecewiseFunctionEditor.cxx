58a1ea77e75b07599f2e30a8e85b4112dbe90df2
barre <barre>
2005-03-20 07:37:16 +0000

    ENH: fix the SetLabel() vs SetText() issue. Some core widgets were using the former, some others the later, both to set the Tk -text option. Also, GetLabel() would sometimes return a vtkKWLabel, or a const char*, alongside a SetLabel(const char*). Not consistent. Now use SetText() for all core widgets. Some composite widget may offer SetLabelText as a shortcut to GetLabel()->SetText()

--- vtkKWPiecewiseFunctionEditor.cxx
+++ vtkKWPiecewiseFunctionEditor.cxx
@@ -26,7 +26,7 @@
 #include "vtkPiecewiseFunction.h"
 
 vtkStandardNewMacro(vtkKWPiecewiseFunctionEditor);
-vtkCxxRevisionMacro(vtkKWPiecewiseFunctionEditor, "$Revision: 1.20 $");
+vtkCxxRevisionMacro(vtkKWPiecewiseFunctionEditor, "$Revision: 1.21 $");
 
 
 int vtkKWPiecewiseFunctionEditorCommand(ClientData cd, Tcl_Interp *interp,
@@ -448,7 +448,7 @@ void vtkKWPiecewiseFunctionEditor::CreateValueEntry(
     this->ValueEntry->SetParent(this->TopRightFrame);
     this->ValueEntry->Create(app, "");
     this->ValueEntry->GetWidget()->SetWidth(6);
-    this->ValueEntry->SetLabel("V:");
+    this->ValueEntry->GetLabel()->SetText("V:");
 
     this->UpdatePointEntries(this->SelectedPoint);
 
