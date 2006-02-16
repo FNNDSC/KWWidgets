21b6dda87cd24ab4e0062cf6f4a4f8717906968a
barre <barre>
2006-02-16 18:55:09 +0000

    ENH: bring in front only if it was never mapped. Other dialogs should be able to come on top after that.

--- vtkKWSplashScreen.cxx
+++ vtkKWSplashScreen.cxx
@@ -23,7 +23,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWSplashScreen );
-vtkCxxRevisionMacro(vtkKWSplashScreen, "$Revision: 1.35 $");
+vtkCxxRevisionMacro(vtkKWSplashScreen, "$Revision: 1.36 $");
 
 //----------------------------------------------------------------------------
 vtkKWSplashScreen::vtkKWSplashScreen()
@@ -243,7 +243,10 @@ void vtkKWSplashScreen::SetProgressMessage(const char *txt)
   this->Script("%s itemconfigure msg -text \"%s\"",
                this->Canvas->GetWidgetName(), (val ? val : ""));
 
-  this->Display();
+  if (!this->IsMapped())
+    {
+    this->Display();
+    }
 }
 
 //----------------------------------------------------------------------------
