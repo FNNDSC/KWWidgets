6ea3cc30d16ec84343e164c6c4da47ab66254e2f
barre <barre>
2006-06-22 20:58:33 +0000

    ENH: make sure directories can be created

--- vtkKWLoadSaveDialog.cxx
+++ vtkKWLoadSaveDialog.cxx
@@ -21,7 +21,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro( vtkKWLoadSaveDialog );
-vtkCxxRevisionMacro(vtkKWLoadSaveDialog, "$Revision: 1.54 $");
+vtkCxxRevisionMacro(vtkKWLoadSaveDialog, "$Revision: 1.55 $");
 
 //----------------------------------------------------------------------------
 vtkKWLoadSaveDialog::vtkKWLoadSaveDialog()
@@ -112,7 +112,7 @@ int vtkKWLoadSaveDialog::Invoke()
     {
     if (support_choose_dir)
       {
-      command << " -mustexist 1";
+      command << " -mustexist 0";
       }
     }
   else
