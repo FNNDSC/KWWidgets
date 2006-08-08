b040d8ef8348430a65086c4cb2d22163407221b9
barre <barre>
2006-08-08 15:23:52 +0000

    ENH: add flag to color the sorted column automatically

--- vtkKWMultiColumnList.cxx
+++ vtkKWMultiColumnList.cxx
@@ -31,7 +31,7 @@
 
 //----------------------------------------------------------------------------
 vtkStandardNewMacro(vtkKWMultiColumnList);
-vtkCxxRevisionMacro(vtkKWMultiColumnList, "$Revision: 1.64 $");
+vtkCxxRevisionMacro(vtkKWMultiColumnList, "$Revision: 1.65 $");
 
 //----------------------------------------------------------------------------
 class vtkKWMultiColumnListInternals
@@ -4065,6 +4065,8 @@ void vtkKWMultiColumnList::PrintSelf(ostream& os, vtkIndent indent)
 {
   this->Superclass::PrintSelf(os,indent);
 
+  os << indent << "ColorSortedColumn: " 
+     << (this->ColorSortedColumn ? "On\n" : "Off\n");
   os << indent << "SortedColumnBackgroundColor: (" 
      << this->SortedColumnBackgroundColor[0] << ", " 
      << this->SortedColumnBackgroundColor[1] << ", " 
