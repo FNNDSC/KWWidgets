96f9e52304a275c120e03fff391f17f7886c4832
barre <barre>
2008-05-13 21:23:09 +0000

    ENH: test VTK 5.2 install

--- Testing/Cxx/TestRandom.cxx
+++ Testing/Cxx/TestRandom.cxx
@@ -1,8 +1,51 @@
 #include "vtkKWEvent.h"
 #include "vtkKWObject.h"
 
+#include "vtkToolkits.h"
+
+/* Let's check if VTK >= 5.2 is exporting its headers correctly */
+
+#if VTK_MAJOR_VERSION > 5 || (VTK_MAJOR_VERSION == 5 && VTK_MINOR_VERSION >= 2)
+
+// MetaIO
+
+#ifdef VTK_USE_METAIO
+#include "vtkmetaio/metaLine.h"
+#endif
+
+// DICOM Parser
+
+#include "DICOMParser.h"
+
+// Verdict
+
+#include "verdict.h"
+
+#endif // VTK_MAJOR_VERSION > 5 || (VTK_MAJOR_VERSION == 5 && VTK_MINOR_VERSION >= 2)
+
 int main()
 {
+#if VTK_MAJOR_VERSION > 5 || (VTK_MAJOR_VERSION == 5 && VTK_MINOR_VERSION >= 2)
+
+  // MetaIO
+
+#ifdef VTK_USE_METAIO
+  vtkmetaio::MetaLine line;
+  (void)line;
+#endif
+
+  // DICOM Parser
+  
+  DICOMParser parser;
+  (void)parser;
+
+  // Verdict
+
+  HexMetricVals hex_metric;
+  (void)hex_metric;
+
+#endif // VTK_MAJOR_VERSION > 5 || (VTK_MAJOR_VERSION == 5 && VTK_MINOR_VERSION >= 2)
+
   int res = 0;
   const char *event = vtkKWEvent::GetStringFromEventId(2001);
   if (!event || strcmp("MessageDialogInvokeEvent", event))
