f686a766916a61065029fc923cc68eb9cede5c09
barre <barre>
2008-07-02 09:45:57 +0000

    STYLE: Nightly Version update

--- vtkKWWidgetsVersion.h
+++ vtkKWWidgetsVersion.h
@@ -25,7 +25,7 @@
 #include "vtkKWWidgets.h" // Needed for export symbols directives
 #include "vtkObject.h"
 
-#define KWWidgets_SOURCE_VERSION "KWWidgets version " KWWidgets_VERSION ", KWWidgets source $Revision: 1.667 $, $Date: 2008-07-01 10:07:01 $ (GMT)"
+#define KWWidgets_SOURCE_VERSION "KWWidgets version " KWWidgets_VERSION ", KWWidgets source $Revision: 1.668 $, $Date: 2008-07-02 09:45:57 $ (GMT)"
 
 class KWWidgets_EXPORT vtkKWWidgetsVersion : public vtkObject
 {
