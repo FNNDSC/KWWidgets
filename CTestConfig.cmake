f940e3e489629d207d446856e34249f29a86efef
barre <barre>
2009-05-18 23:34:23 +0000

    ENH: update for cdash

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -1,6 +1,8 @@
 set(CTEST_PROJECT_NAME "KWWidgets")
 set(CTEST_NIGHTLY_START_TIME "21:30:00 EDT")
+
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "public.kitware.com")
-set(CTEST_DROP_LOCATION "/cgi-bin/HTTPUploadDartFile.cgi")
+set(CTEST_DROP_SITE "www.cdash.org")
+set(CTEST_DROP_LOCATION "/CDash/submit.php?project=KWWidgets")
+set(CTEST_DROP_SITE_CDASH TRUE)
 set(CTEST_TRIGGER_SITE "http://${CTEST_DROP_SITE}/cgi-bin/Submit-KWWidgets-TestingResults.cgi")
