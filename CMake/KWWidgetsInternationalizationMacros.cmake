34042bef2bdccc9d4f64db3ceabcadb8dc3086d9
barre <barre>
2010-03-31 21:24:10 +0000

    ENH: use CMake 2.6 (to avoid more policy issues)

--- CMake/KWWidgetsInternationalizationMacros.cmake
+++ CMake/KWWidgetsInternationalizationMacros.cmake
@@ -115,10 +115,7 @@
 #    CREATE_MO_TARGET was true.
 #    Default to 0.
 
-cmake_minimum_required(VERSION 2.4)
-if(COMMAND cmake_policy)
-  cmake_policy(SET CMP0003 NEW)
-endif(COMMAND cmake_policy)
+cmake_minimum_required(VERSION 2.6)
 
 macro(KWWidgets_CREATE_GETTEXT_TARGETS)
 
